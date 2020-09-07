f7929a8153dde5db90d18ed0c6822877c9dfb053
evaleries <didaarda15@gmail.com>
2020-09-08 02:02:37 +0700

    Fix Wrong total submissions
    As described issue on #28
    Also enhance the query with eager loading.

--- app/Http/Controllers/Assistant/ClassController.php
+++ app/Http/Controllers/Assistant/ClassController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers\Assistant;
 
-use App\ClassAssistant;
-use App\Classes;
-use App\ClassStudent;
-use App\Http\Controllers\Controller;
-use App\Student;
+use App\Task;
 use App\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
+use App\Classes;
+use App\Student;
+use App\ClassStudent;
 use Mockery\Exception;
+use App\ClassAssistant;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class ClassController extends Controller
 {
@@ -58,12 +59,15 @@ class ClassController extends Controller
      * @param \App\Classes $classes
      * @return \Illuminate\Http\Response
      */
-    public function show(Classes $class)
+    public function show($classId)
     {
-        if(Auth::user()->can('view', $class)){
-            return view('dashboard.assistant.class.show', compact('class'));
-        }
-        return abort(403);
+        $class = Classes::with(['students.student.submissions' => function ($q) use ($classId) {
+            return $q->whereIn('task_id', Task::whereClassId($classId)->get()->pluck('id'));
+        }])->findOrFail($classId);
+
+        abort_unless(Auth::user()->can('view', $class), 403);
+
+        return view('dashboard.assistant.class.show', compact('class'));
     }
 
     /**
