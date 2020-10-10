c09e8693a2a2f6c832e21e43928a301a5ba804cf
Miqdad Farcha <miqdad.farcha@gmail.com>
2020-10-10 22:14:05 +0700

    Fix telegram command

--- app/Console/Commands/Telegram/RegisterCommand.php
+++ app/Console/Commands/Telegram/RegisterCommand.php
@@ -27,32 +27,38 @@ class RegisterCommand extends Command
     public function handle()
     {
         $telegram_id = $this->update->message->from->id;
-        $nim = Str::of($this->update->message->text)->explode(' ')->toArray()[1];
+        try {
+            $nim = Str::of($this->update->message->text)->explode(' ')->toArray()[1];
 
-        if (User::where('telegram_id', $telegram_id)->first()) {
+            if (User::where('telegram_id', $telegram_id)->first()) {
+                $this->replyWithMessage([
+                    'text' => "Maaf, akun telegram anda sudah terdaftarkan"
+                ]);
+                return;
+            }
+
+            $user = User::where('username', $nim)->first();
+
+            if ($user) {
+                $pass = Str::random(8);
+                $user->update([
+                    'telegram_id' => $telegram_id,
+                    'password' => Hash::make($pass),
+                    'activate_at' => now()
+                ]);
+
+                $message = "Selamat Datang $user->name 🎉 \n" .
+                    "Password anda untuk login ke " . env('APP_URL') . " : $pass \n" .
+                    "Tolong simpan dan jangan bagikan password ini ke orang lain ❤️";
+                $this->replyWithMessage(['text' => $message]);
+            } else {
+                $this->replyWithMessage([
+                    'text' => "Maaf, NIM tidak ditemukan \n Silahkan hubungi admin @miqdadyyy"
+                ]);
+            }
+        } catch (\Exception $exception){
             $this->replyWithMessage([
-                'text' => "Maaf, akun telegram anda sudah terdaftarkan"
-            ]);
-            return;
-        }
-
-        $user = User::where('username', $nim)->first();
-
-        if ($user) {
-            $pass = Str::random(8);
-            $user->update([
-                'telegram_id' => $telegram_id,
-                'password' => Hash::make($pass),
-                'activate_at' => now()
-            ]);
-
-            $message = "Selamat Datang $user->name 🎉 \n" .
-                "Password anda untuk login ke " . env('APP_URL') . " : $pass \n" .
-                "Tolong simpan dan jangan bagikan password ini ke orang lain ❤️";
-            $this->replyWithMessage(['text' => $message]);
-        } else {
-            $this->replyWithMessage([
-                'text' => "Maaf, NIM tidak ditemukan \n Silahkan hubungi admin @miqdadyyy"
+                'text' => 'Maaf, command anda salah'
             ]);
         }
     }
